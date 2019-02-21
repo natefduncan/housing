6510011170f5c3be8daa95f947be02237ca96507
Nathan Duncan <natefduncan@gmail.com>
2019-02-20 23:30:07 -0600

    edits

--- housing/spiders/housing_spiders.py
+++ housing/spiders/housing_spiders.py
@@ -19,9 +19,10 @@ from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait, ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from pyvirtualdisplay import Display
 
 #FOR SPLASH
