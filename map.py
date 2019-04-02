7e664e90d552abc6f381e9ea19f4743db1f62bad
Nathan Duncan <natefduncan@gmail.com>
2019-04-02 08:10:28 -0500

    edits

--- map.py
+++ map.py
@@ -4,6 +4,7 @@ import plotly.graph_objs as go
 import pandas as pd
 import os
 from pathlib import Path
+import numpy as np
 
 #path = Path(os.path.dirname(os.path.abspath(__file__)))
 path = Path("/Users/User/Desktop/housing/housing/spiders")
@@ -19,13 +20,21 @@ mapbox_access_token = "pk.eyJ1IjoibmF0ZWR1bmNhbiIsImEiOiJjam01Z3kzMGMxcm1uM2trOG
 #Get data.
 data = pd.read_csv("realtor_data_2019.3.31.csv")
 
+scl = "Picnic"
+
 data = [
     go.Scattermapbox(
         lat=data.lat.tolist(),
         lon=data.lon.tolist(),
         mode='markers',
         marker=go.scattermapbox.Marker(
-            size=14
+            size=10,
+            color=data.acres_lot.tolist(),
+            colorscale=scl,
+            cmin=min(data.acres_lot.tolist()),
+            cmax=max(data.acres_lot.tolist()),
+            reversescale=True,
+            opacity=.5
         ),
         text=data.url.tolist(),
     )
@@ -39,7 +48,7 @@ layout = go.Layout(
         bearing=0,
         center=go.layout.mapbox.Center(
             lat=32.9,
-            lon=--96.3
+            lon=-96.3
         ),
         pitch=0,
         zoom=5
--- proxy_rotator.py
+++ proxy_rotator.py
@@ -5,24 +5,26 @@ url = 'http://falcon.proxyrotator.com:51337/'
 apiKey1 = '8nScF6zCADybqLxBMUwYmXgoKetkQT7v'
 apiKey2 = "vVe9qKMBNojcgsPaytGQETmSR3rLDbnX"
 
-params = dict(
-    apiKey=apiKey2,
-    county='US'
-)
-
-proxies = []
-
-for i in range(0, 50):
-    resp = requests.get(url=url, params=params)
-    data = json.loads(resp.text)
-    proxies.append(data['proxy'])
-
 filename = "housing/spiders/proxy_list.txt"
 
-f = open(filename, 'w+')
+for i in [apiKey1, apiKey2]:
 
-for m in proxies:
-    print(m)
-    f.write("%s\n" % m)
-f.close()
+  params = dict(
+      apiKey=i,
+      county='US'
+  )
+  
+  proxies = []
+  
+  for i in range(0, 50):
+      resp = requests.get(url=url, params=params)
+      data = json.loads(resp.text)
+      proxies.append(data['proxy'])
+  
+  f = open(filename, 'a+')
+  
+  for m in proxies:
+      print(m)
+      f.write("%s\n" % m)
+  f.close()
 
