3aa0e10772640d4ed689d94dbda92264e19b5092
Nathan Duncan <natefduncan@gmail.com>
2019-04-09 20:30:07 -0500

    edits

--- housing/spiders/housing_spiders.py
+++ housing/spiders/housing_spiders.py
@@ -515,18 +515,26 @@ class zillow(scrapy.Spider):
     name = "zillow"
         
     def start_requests(self):
-      areas = ["Dallas-County_TX", "Collin-County_TX", "Denton-County_TX", "Rockwall-County_TX", "Hunt-County_TX", "Tarrant-County_TX", 
-      "Johnson-County_TX", "Ellis-County_TX", "Kaufman-County_TX"]
-      base = "https://www.realtor.com/realestateandhomes-search/"
-      url = 'https://www.zillow.com/homes/frisco,-tx_rb/'
-      yield scrapy.Request(url=url, callback=self.parse)
+      areas = ["/dallas-county_rb/", "/Collin-County-TX_rb/", "/Denton-County-TX_rb/", "Rockwall-County-TX_rb", "Hunt-County-TX_rb", 
+      "Tarrant-County-TX_rb", "Johnson-County-TX_rb", "Ellis-County-TX_rb", "Kaufman-County-TX_rb"]
+      base = 'https://www.zillow.com/homes'
+      for i in areas:
+        for j in range(1, 21):
+          url = base + i + "/%s_p" % (str(j))
+          yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
       print("RESPONSE-------")
       url_xpath = "//div[contains(@class, 'zsg-photo-card-content zsg-aspect-ratio-content')]/a[contains(@href, 'homedetails')]/@href'
       request = scrapy.Selector(response)
-      for i in request.xpath(url_xpath).extract():
-        print(i)
+      urls = request.xpath(url_xpath).extract()
+      now = dt.datetime.now()
+      file_name = "zillow_urls_" + str(now.year) + "." + str(now.month) + "."  + str(now.day) + ".txt"
+      for link in urls:
+        with open(file_name, "a+") as file:
+          file.write(link)
+          file.write("\n")
+          print("Added: " + link)
         
 class trulia(scrapy.Spider):
     
