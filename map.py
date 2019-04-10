d26cbaede6aeffa3e2c4d5a71dfbb0eaa82d1385
Nathan Duncan <natefduncan@gmail.com>
2019-04-09 20:04:33 -0500

    edits

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import numpy as np
 
 #path = Path(os.path.dirname(os.path.abspath(__file__)))
-path = Path("/Users/User/Desktop/housing/housing/spiders")
+path = Path("/Users/User/Desktop/housing")
 os.chdir(path)
 
 #Connect to Plotly API
@@ -18,25 +18,59 @@ plotly.tools.set_credentials_file(username=username, api_key=api_key)
 mapbox_access_token = "pk.eyJ1IjoibmF0ZWR1bmNhbiIsImEiOiJjam01Z3kzMGMxcm1uM2trOGM1c2x6ZnVsIn0.frI_rzsE819mtoRhxt4m8w"
 
 #Get data.
-data = pd.read_csv("realtor_data_2019.3.31.csv")
+data = pd.read_csv("data.csv")
 
 scl = "Picnic"
 
+desc = []
+data.columns
+for i,j,k,l,m,n,o,p,q,r,s in zip(data.url, data.price, data.beds, data.baths, 
+                         data.half_baths, data.sq_ft, data.acres_lot,
+                         data.on_realtor, data.built, data.preds, data.expected_diff):
+    desc.append(
+            '''
+            Url: %s
+            Price: %s
+            Beds: %s
+            Full Bath: %s
+            Half Bath: %s
+            Square Feet: %s
+            Acres: %s
+            On the Market: %s
+            Built: %s
+            Predicted Price: %s
+            Difference w/ Actual: %s
+            ''' % (str(i), str(j),str(k),str(l),
+            str(m),str(n),str(o),str(p),str(q),str(r),str(s))
+            )
+
+data['desc'] = desc
+data_good = data[(data.expected_diff < 0) & (data.price < 250000) & (data.type == 'Single Family Home')]
+data_bad = data[(data.expected_diff > 0) & (data.price < 250000) & (data.type == 'Single Family Home')]
+from scipy.stats import percentileofscore
+
 data = [
     go.Scattermapbox(
-        lat=data.lat.tolist(),
-        lon=data.lon.tolist(),
+        lat=data_good.lat.tolist(),
+        lon=data_good.lon.tolist(),
         mode='markers',
         marker=go.scattermapbox.Marker(
-            size=10,
-            color=data.acres_lot.tolist(),
-            colorscale=scl,
-            cmin=min(data.acres_lot.tolist()),
-            cmax=max(data.acres_lot.tolist()),
-            reversescale=True,
+            size=[percentileofscore(data_good.expected_diff.tolist(), i)*10/100 + 5 for i in data_good.expected_diff.tolist()],
+            color='green',
             opacity=.5
         ),
-        text=data.url.tolist(),
+        text=data_good.desc.tolist()
+    ),
+    go.Scattermapbox(
+        lat=data_bad.lat.tolist(),
+        lon=data_bad.lon.tolist(),
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=[percentileofscore(data_bad.expected_diff.tolist(), i)*10/100 + 5 for i in data_bad.expected_diff.tolist()],
+            color='red',
+            opacity=.5
+        ),
+        text=data_bad.desc.tolist()
     )
 ]
 
--- realtor_analysis.py
+++ realtor_analysis.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from pathlib import Path
 import os
 import numpy as np
@@ -9,24 +8,6 @@ os.getcwd()
 os.chdir(Path("/Users/User/Desktop/housing/housing/spiders"))
 
 data = pd.read_csv("realtor_data_2019.3.31.csv")
-y = data.price
-
-columns = ["city", "state", "zip", "beds", "baths", "half_baths", "sq_ft", "sqft_lot", "acres_lot", 
-           "status", "price_sq_ft", "on_realtor", "type", "built", "style"]
-
-X =  data[columns]
-
-#Encode city, state, zip, type, style
-from sklearn.preprocessing import OneHotEncoder
-
-labelencoder = LabelEncoder()
-
-X['city'] = labelencoder.fit_transform(X['city'].astype(str))
-X['state'] = labelencoder.fit_transform(X['state'].astype(str))
-X['zip'] = labelencoder.fit_transform(X['zip'].astype(str))
-X['status'] = labelencoder.fit_transform(X['status'].astype(str))
-X['type'] = labelencoder.fit_transform(X['type'].astype(str))
-X['style'] = labelencoder.fit_transform(X['style'].astype(str))
 
 def get_ints(x): #Gets only the numbers from a string. 
     x = str(str(x).split(".")[0])
@@ -39,17 +20,108 @@ def get_ints(x): #Gets only the numbers from a string.
             pass
     return "".join(output)
 
-X = X.replace("", np.nan)
-X.loc[:, ["price_sq_ft"]] = [float(get_ints(i)) for i in X.price_sq_ft.tolist()]
-X.loc[:, ["on_realtor"]] = [float(get_ints(i)) for i in X.on_realtor.tolist()]
+data = data.replace("No Info", np.nan)
+data['on_realtor'] = [get_ints(i) for i in data['on_realtor'].tolist()]
+urls = data.url.tolist()
+house_type = data.type.tolist()
+
+data[['on_realtor']] = data[['on_realtor']].apply(pd.to_numeric, args=('coerce',))
+        
+#Sq ft to acres
+acre_conversion = 43560 
+
+acre_lot = []
+
+for i,j in zip(data.sqft_lot.tolist(), data.acres_lot.tolist()):
+    if ~np.isnan(i):
+        acre_lot.append(float(i)/acre_conversion)
+    else:
+        acre_lot.append(j)
+
+data['acres_lot'] = acre_lot
+
+data = data.drop(['sqft_lot', 'price_sq_ft'], axis=1)
+
+#Encode city, state, zip, type, style
+from sklearn.preprocessing import LabelEncoder
+
+labelencoder = LabelEncoder()
+
+city_en =LabelEncoder()
+state_en =LabelEncoder()
+zip_en = LabelEncoder()
+status_en =LabelEncoder()
+type_en =LabelEncoder()
+style_en =LabelEncoder()
+
+data['city'] = city_en.fit_transform(data['city'].astype(str))
+data['state'] = state_en.fit_transform(data['state'].astype(str))
+data['zip'] = zip_en.fit_transform(data['zip'].astype(str))
+data['status'] = status_en.fit_transform(data['status'].astype(str))
+data['type'] = type_en.fit_transform(data['type'].astype(str))
+data['style'] = style_en.fit_transform(data['style'].astype(str))
+
+keep_cols = ['lat', 'lon', 'url', 'city', 'state', 'zip', 'status', 'type', 'style','price', "beds", "baths", "half_baths", "sq_ft", "acres_lot",  "on_realtor", "built"]
+
+data=data[keep_cols]
+data.url = urls
+
+#half baths
+where_nan = [np.isnan(i) for i in data.half_baths.tolist()]
+data['half_baths'][where_nan] = 0 
+
+data = data.dropna()
+
+y = data.price
+
+columns = ['city', 'state', 'zip', 'status', 'type', 'style', "beds", "baths", "half_baths", "sq_ft", "acres_lot",  "on_realtor", "built"]
+
+X =  data[columns]
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
+scaler = StandardScaler()
+# Fit only to the training data
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+X[columns] = scaler.transform(X[columns])
+
+from sklearn.neural_network import MLPRegressor
+
+mlp = MLPRegressor(hidden_layer_sizes=(30,30,30))
+
+mlp.fit(X_train,y_train)
+
+predictions = mlp.predict(X_test)
+mlp.score(X_test, y_test)
+mlp.get_params()
+
+from sklearn.metrics import r2_score, explained_variance_score
+print(r2_score(y_test,predictions))
+print(explained_variance_score(y_test, predictions))
+
+import seaborn as sns
+
+sns.lmplot("x", "y", data=pd.DataFrame({"x":y_test, "y" : predictions}))
+
+
+preds = mlp.predict(X)
+
+data['preds'] = preds
+
+data['expected_diff'] = [i-j for i,j in zip(data.price.tolist(), data.preds.tolist())]
+
+sns.distplot(data['expected_diff'])
+
+data['city'] = list(city_en.inverse_transform(data['city']))
+data['state'] = state_en.inverse_transform(data['state'])
+data['zip'] = zip_en.inverse_transform(data['zip'])
+data['status'] = status_en.inverse_transform(data['status'])
+data['type'] = type_en.inverse_transform(data['type'])
+data['style'] = style_en.inverse_transform(data['style'])
+
+data.to_csv("../../data.csv")
 
